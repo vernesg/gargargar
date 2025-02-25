e84eb897a4d5ebed66f464e3bfc8c52ecc949f5d
Aminul-sordar <132671015+Aminulsordar@users.noreply.github.com>
2025-02-25 10:49:40 +0600

    Update up.js

--- script/up.js
+++ script/up.js
@@ -7,7 +7,7 @@ module.exports["config"] = {
     role: 0,
     credits: "aminul",
     description: "Displays uptime and system information",
-    hasPrefix: false,
+    hasPrefix: true,
     cooldowns: 5,
     aliases: ["up"]
 };

7855adb836089d6fd2d09c93d28f59daa9d536ba
oh-notfound404 <kiddryee@gmail.com>
2025-06-05 02:08:34 +0800

    Update autoreact.js

--- script/event/autoreact.js
+++ script/event/autoreact.js
@@ -1,39 +1,19 @@
- module.exports = {
-  config: {
-    name: "autoreact",
-    author: "Aminulsordar",
-    version: "1.0",
-    countDown: 5,
-    role: 0,
-    shortDescription: "Auto React",
-    longDescription: " ",
-    category: "System",
-  },
-  onStart: async function () {
-    // Add initialization logic here if needed
-  },
-  onChat: async function ({ event, api }) {
-    const message = event.body.toLowerCase();
+const axios = require('axios');
+const path = require('path');
+const fs = require('fs');
+const request = require('request');
 
-    const reactionsMap = {
-      "😂": ["haha", "lol", "funny", "hahah", "hahaha", "masaya", "happy", "🤣", "natomba", "tumomba", "tomomba", "tumumba", "tomumba", "side eye", "awooop jumpscare", "so masaya ka?", "sana all"],
-      "😭": ["cry", "sad", "crying", "bakit ka malungkot?", "bakit ka malongkot?", "hindi na", "sad ka", "walang ulam"],
-      "🥰": ["love", "mahal", "crush"],
-      "🎮": ["laro", "laru", "game", "mc", "minecraft", "ml", "mlbb", "mobile legends", "mobile legends bang bang", "cod", "call of duty"]
-      // Add more reactions and associated keywords as needed
-    };
+module.exports.config = {
+  name: "randomreaction",
+  version: "69",
+  credits: "cttro",
+};
 
-    console.log("Message:", message);
+module.exports.handleEvent = async function ({ api, event }) {
+  if (event.body) {
+    const emojis = ['🫡', '😝', '😠', '🤣', '😍', '🥹', '🤖'];
+    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
 
-    for (const [reaction, keywords] of Object.entries(reactionsMap)) {
-      console.log("Reaction:", reaction);
-      console.log("Keywords:", keywords);
-
-      if (keywords.some((word) => message.includes(word))) {
-        console.log("Reacting with:", reaction);
-        api.setMessageReaction(reaction, event.messageID, event.threadID, api);
-        break; // Stop checking once a reaction is set
-      }
-    }
-  },
+    api.setMessageReaction(randomEmoji, event.messageID, () => {}, true);
+  }
 };

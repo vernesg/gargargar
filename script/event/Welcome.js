9f962c0ba07d6c52c3564faf35de97c3b77d23b4
oh-notfound404 <kiddryee@gmail.com>
2025-06-05 10:14:25 +0800

    Update Welcome.js

--- script/event/Welcome.js
+++ script/event/Welcome.js
@@ -31,7 +31,7 @@ module.exports.handleEvent = async function ({ api, event }) {
 
         // Build the welcome image API URL
         const apiKey = "86397083-298d-4b97-a76e-414c1208beae";
-        const url = `https://kaiz-apis.gleeze.com/api/welcome?username=${encodeURIComponent(name)}&avatarUrl=${encodeURIComponent(avatarUrl)}&groupname=${encodeURIComponent(groupName)}&bg=${encodeURIComponent(background)}&memberCount=${memberCount}&apikey=${apiKey}`;
+        const url = `https://kaiz-apis.gleeze.com/api/welcome?username=${encodeURIComponent(name)}&avatarUrl=https://scontent.xx.fbcdn.net/v/t1.15752-9/455020392_3836507706596463_5673950550239467059_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=108&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeGFfR2_seWRZjgzQTsyPs17xlJdpLxTcrfGUl2kvFNyt_c7thKkpQ9_dpQdgcPcj9_Guio2y0TBJkRcYaP6NgmM&_nc_ohc=LRE6ktZDCd8Q7kNvwFwlWH5&_nc_oc=AdnUYCAb4LdWco0Fl0F9fQPsrAPaKdSIlqsQeH7knHJ1EmdF0olKR7rykT44O5R6hPc&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD2gEiBdeK1ltIZrDBf0g-eOTuNkqL46jCj8mMfpOQIFGOvQ&oe=6868610D&groupname=${encodeURIComponent(groupName)}&bg=${encodeURIComponent(background)}&memberCount=${memberCount}&apikey=${apiKey}`;
 
         try {
             const { data } = await axios.get(url, { responseType: 'arraybuffer' });
